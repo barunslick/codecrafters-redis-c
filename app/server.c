3b9efeba29a6d33d6dda4641757e4145b545ebe4
barunslick <barunpradhan987@gmail.com>
2024-11-19 22:29:09 +0545

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -7,6 +7,36 @@
 #include <errno.h>
 #include <unistd.h>
 
+
+const int DEFAULT_REDIS_PORT = 6379;
+
+void error(char * msg){
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(1);
+}
+
+int create_server_socket() {
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+		error("Socket creation failed");
+
+	return server_fd;
+}
+
+void bind_to_port(int socket, int port, int reuse) {
+	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+					 .sin_port = htons(port),
+					 .sin_addr = { htonl(INADDR_ANY) },
+					};
+
+
+	if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+		error("SO_REUSEADDR failed");
+
+	if (bind(socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0)
+		error("Bind failed");
+}
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
@@ -20,29 +50,12 @@ int main() {
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
+	server_fd = create_server_socket();
 
 	// Since the tester restarts your program quite often, setting SO_REUSEADDR
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-					 .sin_port = htons(6379),
-					 .sin_addr = { htonl(INADDR_ANY) },
-					};
-
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
+	bind_to_port(server_fd, DEFAULT_REDIS_PORT, reuse);
 
 	int connection_backlog = 5;
 	if (listen(server_fd, connection_backlog) != 0) {
